f5fb0ba32ee0aaa61aa003d2ff8c40e272a09eb5
Daniel Wolf <danielrwolf5@gmail.com>
2018-10-20 23:16:04 -0500

    edge case handling in get me controller

--- src/Controller/GetMeAction.php
+++ src/Controller/GetMeAction.php
@@ -3,12 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * Class GetMeAction
+ * @package App\Controller
+ */
 class GetMeAction
 {
+    /**
+     * @param UserInterface $user
+     *
+     * @throws AccessDeniedException
+     * @return User
+     */
     public function __invoke(UserInterface $user): User
     {
+        if (!$user instanceof User) {
+            throw new AccessDeniedException();
+        }
+
         return $user;
     }
 }
